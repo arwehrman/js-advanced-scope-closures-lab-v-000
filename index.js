c4fd245e3df5f80e95750e96e530d2ebf823a3fd
Amanda Wehrman <arwehrman@gmail.com>
2018-01-28 01:55:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(blockRange) {
+  return function(begin, end) {
+    let distance = parseInt(end) - parseInt(begin)
+      if ( distance < blockRange ) {
+        return `within range by ${Math.abs(blockRange - distance)}`
+      }
+      else {
+        return `${Math.abs(blockRange - distance)} blocks out of range`
+      }
+    }
+  }
+
+function produceTipCalculator(fare){
+  return function(rate){
+    return fare * rate
+  }
+}
