db73f4269a547129964b1a15b3853dba9d21dcb2
Amanda Wehrman <arwehrman@gmail.com>
2018-01-28 01:53:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-function produceDrivingRange(blockRange){
-  return function(a, b){
-    let start = parseInt(a)
-    let end = parseInt(b)
-    let distance = end - start
-    let difference = blockRange - distance
-
-    if( difference > 0){
-      return `within range by ${difference}`
-    }
-    else {
-      return `${Math.abs(difference)} blocks out of range`
+function produceDrivingRange(blockRange) {
+  return function(begin, end) {
+    let distance = parseInt(end) - parseInt(begin)
+      if ( distance < blockRange ) {
+        return `within range by ${Math.abs(blockRange - distance)}`
+      }
+      else {
+        return `${Math.abs(blockRange - distance)} blocks out of range`
+      }
     }
   }
-}
 
 function produceTipCalculator(fare){
   return function(tip){
